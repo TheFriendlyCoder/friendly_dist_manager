f4bdda245731324c571273d5bcca7d699415b7e7
Kevin Phillips <thefriendlycoder@gmail.com>
2021-03-07 11:43:12 -0400

    Added basic test framework to project

---
+++ tests/test_api.py
@@ -0,0 +1,21 @@
+from .utils import run_pip
+from pathlib import Path
+import pytest
+
+
+@pytest.mark.skip(reason="To be fixed")
+def test_wheel():
+    sample_toml = """
+        [build-system]
+        requires = ['wheel']
+        build-backend = "friendly_dist_manager.api:PEP517"
+    """
+
+    pyproj = Path("pyproject.toml")
+    pyproj.write_text(sample_toml)
+    proj_dir = Path("sample")
+    proj_dir.mkdir()
+    init_file = proj_dir / "__init__.py"
+    init_file.write_text("__version__='1.0.0'")
+    args = "install --no-build-isolation ."
+    run_pip(args)
